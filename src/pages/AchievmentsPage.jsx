22b77847d901c606b5141f5c22dbb257017e2f71
Hypnoticaxx <justanotherawesomeguytoday@gmail.com>
2025-12-04 06:41:11 -0500

    Create AchievmentsPage.jsx

---
+++ src/pages/AchievmentsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AchievementList from '../components/Achievements/AchievementList';
+
+export default function AchievementsPage() {
+  return (
+    <div>
+      <AchievementList />
+    </div>
+  );
+}
